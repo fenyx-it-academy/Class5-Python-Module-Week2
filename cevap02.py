8b846bdb06f4eaf399cc9e819402bc04a5ab2bb9
adil Yunus <adil.yunus@gmail.com>
2021-09-24 19:50:50 +0200

    massage

---
+++ cevap02.py
@@ -0,0 +1,9 @@
+print('aaa')
+# git status
+# git add cevap1.py
+# git add cevap2.py
+# (git add .)
+# git status - kontrol komutu
+# git commit -m "2.haftanin odevlerini icerir"
+# git status
+# git push -u origin main
