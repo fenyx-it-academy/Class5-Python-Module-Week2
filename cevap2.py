fdaec7e133f01140404eda72bd1d4a68c122f563
sigma21 <semihkocakel@hotmail.com>
2021-09-24 19:52:13 +0200

    2.hafta 2.odev eklemesi

--- cevap2.py
+++ cevap2.py
@@ -1,1 +1,13 @@
 print('cevap 2')
+
+
+# git status
+# git add cevap1.py
+# git add cevap2.py
+# (git add .)
+# git status - kontrol komutu
+# git commit -m "2.haftanin odevlerini icerir"
+# git status
+# git push -u origin main
+
+
